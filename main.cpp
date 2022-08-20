ef08128063c8ab1e8783002dc66b70a9f1952feb
camiielle <camillalazzati@gmail.com>
2022-08-20 11:02:45 +0200

    Included Lyra, header only library

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "parameters.hpp"
+#include <lyra/lyra.hpp>
+
 
 int main(int argc, char* argv[])
 {
   try {
     // Parameters const pars{...}; const rende l'intera variabile class const
 
-  } catch (std::runtime_error const& err) {
+  } catch (std::runtime_error const& e) {
   }
 }
